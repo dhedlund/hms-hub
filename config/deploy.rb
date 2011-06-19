6de84d3b147c0851b7e4e7467c4e12e76b66a703
davehill <hungrything@gmail.com>
2011-06-19 13:48:57 +0200

    First draft of capistrano deploy

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+require 'bundler/capistrano'
+require 'capistrano/ext/multistage' 
+set :stages, %w(internal external)
+
+set :application, "hms-hub"
+
+set :scm, :git
+set :repository,  "git://github.com/dhedlund/hms-hub.git"
+set :branch, 'production'
+set :git_shallow_clone, 1
+set :scm_verbose, true
+
+set :user, 'meduser'
+set :deploy_to, '/var/www/apps/#{application}'
+
+set :shared_files, ["config/priv"]
+
+# Passenger controls:
+namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+end
