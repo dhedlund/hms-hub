d4c7b132b662f2f438168abb066a523e419029e9
Daniel Hedlund <daniel@digitree.org>
2011-07-13 15:19:03 -0700

    Admin: hierarchical navigation

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -14,4 +14,29 @@ module AdminHelper
     nav.select { |i| i[:name] == selected }[0][:selected] = true
     nav
   end
+
+  def nav_hierarchy(hierarchy)
+    paths = Hash[primary_nav.map { |n| [n[:name], n[:path]] }]
+    hierarchy.map do |node|
+      case node
+      when Symbol
+        title = node.to_s.titleize
+        paths[node] ? link_to(title, paths[node]) : title
+      when MessageStream
+        link_to("Stream: #{node.title}", [:admin, node])
+      when Message
+        link_to("Message: #{node.title}", [:admin, node.message_stream, node])
+      when Notifier
+        link_to("Notifier: #{node.username}", [:admin, node])
+      when Notification
+        link_to("Notification: #{node.id}", [:admin, node])
+      when DeliveryAttempt
+        link_to("Attempt: #{node.id}", [:admin, node])
+      when User
+        link_to("User: #{node.username}", [:admin, node])
+      else
+        node
+      end
+    end
+  end
 end
