6c73a472c352fe6bc614d38c714ad2a118ba3de9
Daniel Hedlund <daniel@digitree.org>
2011-08-11 08:28:06 -0700

    Jobs: handle scenarios for requeuing notification delivery jobs

--- app/jobs/deliver_notification_job.rb
+++ app/jobs/deliver_notification_job.rb
@@ -1,8 +1,26 @@
 class DeliverNotificationJob < Struct.new(:notification_id)
   def perform
     notification = Notification.find(notification_id)
-    attempt = DeliveryAttempt.new(:notification => notification)
-    attempt.save
+    attempt = notification.delivery_attempts.last
+
+    # attempt delivery if never attempted or last was temporary failure
+    if !attempt || attempt.result == DeliveryAttempt::TEMP_FAIL
+      attempt = DeliveryAttempt.new(:notification => notification)
+      attempt.save
+    end
+
+    case attempt.result
+    when DeliveryAttempt::TEMP_FAIL
+      # raise an error so job will be retried later, tracking # of retries
+      raise DeliveryAttempt::TEMP_FAIL
+
+    when DeliveryAttempt::ASYNC_DELIVERY
+      # enqueue job to check later after response returned from remote server
+      Delayed::Job.enqueue(DeliverNotificationJob.new(notification.id))
+
+    else # DELIVERED or PERM_FAIL
+      # job should not be retried
+    end
   end
 
 end
--- test/unit/jobs/deliver_notification_job_test.rb
+++ test/unit/jobs/deliver_notification_job_test.rb
@@ -3,29 +3,98 @@ require 'mocha'
 
 class DeliverNotificationJobTest < ActiveSupport::TestCase
   setup do
-    @notification_id = 874
-    @job = DeliverNotificationJob.new(@notification_id)
+    @notification = Factory.create(:notification)
+    @job = DeliverNotificationJob.new(@notification.id)
   end
 
   test "can pass notification_id to constructor" do
-    assert DeliverNotificationJob.new(@notification_id)
+    assert DeliverNotificationJob.new(@notification.id)
   end
 
   test "can access notification_id that was passed to constructor" do
-    assert_equal @notification_id, @job.notification_id
+    assert_equal @notification.id, @job.notification_id
   end
 
   test "responds to: perform" do
     assert_respond_to @job, :perform
   end
 
-  test "calling perform creates and saves (delivers) a new delivery attempt" do
-    notification = Factory.create(:notification, :id => @notification_id)
-
-    attempt = mock()
-    attempt.expects(:save).once
+  test "calling perform on new notification should create a new delivery attempt" do
+    attempt = mock_attempt(@notification)
+    attempt.expects(:save).returns(true)
     DeliveryAttempt.expects(:new).once.returns(attempt)
     @job.perform
+  end
+
+  test "calling perform after TEMP_FAIL should create attempt and try again" do
+    @notification.stubs(:delivery_attempts).returns([mock_attempt(@notification, :temp_fail)])
+    Notification.stubs(:find).returns(@notification)
+
+    attempt = mock_attempt(@notification)
+    DeliveryAttempt.expects(:new).once.returns(attempt)
+    attempt.expects(:save).returns(true)
+
+    @job.perform
+  end
+
+  test "calling perform after ASYNC_DELIVERY should not attempt delivery" do
+    @notification.stubs(:delivery_attempts).returns([mock_attempt(@notification, :async_delivery)])
+    Notification.stubs(:find).returns(@notification)
+    DeliveryAttempt.expects(:new).never
+    @job.perform
+  end
+
+  test "calling perform after DELIVERED should not attempt delivery" do
+    @notification.stubs(:delivery_attempts).returns([mock_attempt(@notification, :delivered)])
+    Notification.stubs(:find).returns(@notification)
+    DeliveryAttempt.expects(:new).never
+    @job.perform
+  end
+
+  test "calling perform after PERM_FAIL should not attempt delivery" do
+    @notification.stubs(:delivery_attempts).returns([mock_attempt(@notification, :perm_fail)])
+    Notification.stubs(:find).returns(@notification)
+    DeliveryAttempt.expects(:new).never
+    @job.perform
+  end
+
+  test "TEMP_FAIL should cause perform to raise an error (fail the job)" do
+    DeliveryAttempt.stubs(:new).returns(mock_attempt(@notification, :temp_fail))
+    assert_raise(RuntimeError) { @job.perform }
+  end
+
+  test "ASYNC_DELIVERY should cause perform to enqueue a new job" do
+    DeliveryAttempt.stubs(:new).returns(mock_attempt(@notification, :async_delivery))
+    assert_difference('Delayed::Job.count') { @job.perform }
+  end
+
+  test "DELIVERED should cause perform to not enqueue a new job" do
+    DeliveryAttempt.stubs(:new).returns(mock_attempt(@notification, :delivered))
+    assert_no_difference('Delayed::Job.count') { @job.perform }
+  end
+
+  test "PERM_FAIL should cause perform to not enqueue a new job" do
+    DeliveryAttempt.stubs(:new).returns(mock_attempt(@notification, :perm_fail))
+    assert_no_difference('Delayed::Job.count') { @job.perform }
+  end
+
+
+  protected
+
+  def mock_attempt(notification, result=nil)
+    r = case result
+    when :delivered then DeliveryAttempt::DELIVERED
+    when :temp_fail then DeliveryAttempt::TEMP_FAIL
+    when :perm_fail then DeliveryAttempt::PERM_FAIL
+    when :async_delivery then DeliveryAttempt::ASYNC_DELIVERY
+    else result
+    end
+
+    attempt = mock()
+    attempt.stubs(:notification).returns(notification)
+    attempt.stubs(:save).returns(true)
+    attempt.stubs(:result).returns(r)
+    attempt
   end
 
 end
